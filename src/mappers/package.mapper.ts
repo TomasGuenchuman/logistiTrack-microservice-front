9df74009772b17af5eface767a57bf2113341edf
igna-gnzlz <gonzalezignacio0404@gmail.com>
2026-06-04 19:20:11 -0300

    feature: agrego mapper que se usará en los servicios para mapear el dato del back al paquete que usará el front

---
+++ src/mappers/package.mapper.ts
@@ -0,0 +1,16 @@
+import { PackageAPIResponse } from "@/types/api/PackageAPIResponse";
+import { Package } from "@/types/domain/Package";
+
+export const mapPackageFromAPI = (raw: PackageAPIResponse): Package => ({
+  id: raw.id,
+  trackingCode: raw.tracking_code,
+  recipientName: raw.recipient_name,
+  recipientDocument: raw.recipient_document,
+  address: raw.address,
+  addressDetail: raw.address_detail,
+  status: raw.status,
+  courierId: raw.courier_id,
+  deliveredAt: raw.delivered_at,
+  createdAt: raw.created_at,
+  updatedAt: raw.updated_at,
+});
