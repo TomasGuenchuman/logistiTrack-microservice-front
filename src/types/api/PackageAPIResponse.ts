9604bb77e033edb435b6f5f3cbab6451c2ff1d64
igna-gnzlz <gonzalezignacio0404@gmail.com>
2026-06-10 00:22:58 -0300

    fix: usar camelCase en el tipo de la api ya que así viene del json

--- src/types/api/PackageAPIResponse.ts
+++ src/types/api/PackageAPIResponse.ts
@@ -3,14 +3,14 @@ import { PackageStatus } from "@/types/PackageStatus";
 // reflejo exactamente la entidad real que viene del backend
 export type PackageApiResponse = {
   id: string;
-  tracking_code: string;
-  recipient_name: string;
-  recipient_document: string;
+  trackingCode: string;
+  recipientName: string;
+  recipientDocument: string;
   address: string;
   status: PackageStatus;
-  address_detail: string | null;
-  courier_id: string | null;
-  delivered_at: string | null;
-  created_at: string;
-  updated_at: string;
+  addressDetail: string | null;
+  courierId: string | null;
+  deliveredAt: string | null;
+  createdAt: string;
+  updatedAt: string;
 };
