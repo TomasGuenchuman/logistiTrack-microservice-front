6479d2b7f115bde8931f350ff2f621cb05900735
igna-gnzlz <gonzalezignacio0404@gmail.com>
2026-06-17 18:02:08 -0300

    feature: obtener paquetes solo si el user está autenticado, permitiendo arreglar algunos bugs

--- src/hooks/usePackages.ts
+++ src/hooks/usePackages.ts
@@ -1,3 +1,4 @@
+import { useAuth } from "@/context/AuthContext";
 import { packageService } from "@/services/index";
 import { Package } from "@/types/domain/Package";
 import { useFocusEffect } from "expo-router";
@@ -5,25 +6,34 @@ import { useCallback, useState } from "react";
 
 export function usePackages(courierId?: string) {
   const [packages, setPackages] = useState<Package[]>([]);
-  // Este contador nos va a servir como un "gatillo" manual para forzar la recarga
-  const [refreshTrigger, setRefreshTrigger] = useState(0);
+  const { isAuthenticated } = useAuth();
 
-  // Sacamos la función afuera
   const fetchPackages = useCallback(async () => {
+    // Si no hay sesión o usuario, no se hace la petición GET de packages,
+    // simplemente limpiamos el estado de los paquetes
+    if (!isAuthenticated || !courierId) {
+      setPackages([]);
+      return;
+    }
+
     try {
       const data = await packageService.getPackagesByCourierId(courierId);
       setPackages(data);
-      console.log("Paquetes recargados desde el hook");
     } catch (error) {
-      console.error("Error al recargar paquetes desde el hook:", error);
+      console.error("Error al recargar paquetes desde el hook, error:", error);
     }
-  }, []);
+    // Obtenemos los paquetes cada vez que la sesión o el usuario cambia
+  }, [isAuthenticated, courierId]);
 
   // Reacción por navegación. Mantiene la lista fresca si el repartidor cambia de pantalla
   useFocusEffect(
     useCallback(() => {
+      // aunque la navegación cambie, impido que se haga la petición
+      // debido a que no hay sesión activa
+      if (!isAuthenticated) return;
+
       fetchPackages();
-    }, [fetchPackages]),
+    }, [fetchPackages, isAuthenticated]),
   );
 
   return {
